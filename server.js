57d3397877fdf611622428e14a91a3b16ce7675b
dinkleva <dinkleva@gmail.com>
2021-08-10 01:38:51 +0530

    pushing to heroku

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set('view engine', 'ejs');
 //const dbURI = process.env.MONGODB_URI
 //const dbURI = "mongodb+srv://dinkleva_123:dinkleva123@cluster0.xhi9f.mongodb.net/gaurav-database?retryWrites=true&w=majority"
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => app.listen(3000))
+.then((result) => app.listen(process.env.PORT || 3000))
 .catch((err) => console.log(err));
 
 // mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
