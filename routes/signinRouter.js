e964f5bbeb7076f0bca1a7d4af45018ada196956
dinkleva <dinkleva@gmail.com>
2021-08-11 02:15:58 +0530

    after login success

--- routes/signinRouter.js
+++ routes/signinRouter.js
@@ -4,7 +4,9 @@ const User = require('../models/user')
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
+const Blog = require('../models/blogs');
 
+const blogs = Blog.find().sort({createdAt: -1})
 
 router.get('/', (req, res, next) =>{
     res.render('signin', {title: 'Login'})
@@ -19,7 +21,7 @@ router.post('/verification', async (req, res) =>{
     }
     try{
         if(await bcrypt.compare(req.body.password, user.password)){
-            res.redirect('/blogs')
+            res.render('blogs/index', {title: 'All Blogs' , blogs: blogs, username: user.name})
         }
         else{
             res.send('Not Allowed')
